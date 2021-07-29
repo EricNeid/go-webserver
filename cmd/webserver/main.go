c10102ed813d97bbc755b0b01171c4c730988fd0
Eric Neidhardt <eric.neidhardt@dlr.de>
2021-07-29 11:32:12 +0200

    Refactor log creation

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -2,49 +2,60 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 
 	"github.com/EricNeid/go-webserver/server"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var (
 	listenAddr string = ":5000"
 )
 
-func readEnvironmentVariables() {
+func init() {
 	value, isSet := os.LookupEnv("LISTEN_ADDR")
 	if isSet {
 		listenAddr = value
 	}
-}
 
-func readCli() {
 	flag.StringVar(&listenAddr, "listen-addr", listenAddr, "server listen address")
 	flag.Parse()
+
+	log.SetFlags(log.LstdFlags)
+	log.SetPrefix("http: ")
+	log.SetOutput(os.Stdout)
+
+	log.SetOutput(
+		io.MultiWriter(
+			os.Stdout,
+			&lumberjack.Logger{
+				Filename:   "log/webserver.log",
+				MaxSize:    500, // megabytes
+				MaxBackups: 3,
+				MaxAge:     28, //days
+			},
+		),
+	)
 }
 
 func main() {
-	readEnvironmentVariables()
-	readCli()
-
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-
 	done := make(chan bool, 1)
 	quit := make(chan os.Signal, 1)
 
 	signal.Notify(quit, os.Interrupt)
 
-	server := server.NewApplicationServer(logger, listenAddr)
+	server := server.NewApplicationServer(log.Default(), listenAddr)
 	go server.GracefullShutdown(quit, done)
 
-	logger.Println("Server is ready to handle requests at", listenAddr)
+	log.Println("Server is ready to handle requests at", listenAddr)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
+		log.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
 	}
 
 	<-done
-	logger.Println("Server stopped")
+	log.Println("Server stopped")
 }
